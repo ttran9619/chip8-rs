0f52f5e041a76356d971a7cc78f33f7a02b3750b
Tyler Tran <ttran9619@gmail.com>
2022-11-23 18:48:20 -0600

    emulator: Add initial structure
    Add initial emulator structure with basic types and machine state.

---
+++ src/emulator.rs
@@ -0,0 +1,21 @@
+mod types;
+
+struct Emulator {
+    // Internal State
+    program_counter: u16,
+    stack_pointer: usize,
+    stack: Stack,
+
+    // Program Accessible
+    i_register: u16,
+    v_registers: RegisterBank,
+    sound_timer: u8,
+    delay_timer: u8,
+
+    // Memory
+    memory: Memory,
+}
+
+type Memory = [u8; 4096];
+type RegisterBank = [u8; 16];
+type Stack = [u16; 16];
---
+++ src/emulator/types.rs
@@ -0,0 +1,5 @@
+pub struct FourBitValue(u8);
+pub struct EightBitValue(u8);
+pub struct TwelveBitValue(u16);
+pub struct RegisterNumber(FourBitValue);
+pub struct MemoryAddress(TwelveBitValue);
